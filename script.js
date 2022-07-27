81ac534b9d0091d2d17c9d3b31404001f1933b00
TheShrix <devyncloss@hotmail.com>
2022-07-27 05:37:23 -0400

    added data tags to elements in html. defined variables in script

---
+++ script.js
@@ -0,0 +1,10 @@
+const operandButton = document.querySelectorAll('[data-operand]');
+const operatorButton = document.querySelectorAll('[data-operator]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-allClear]');
+const previousOpText = document.querySelector('[data-previousOp]');
+const currentOpText = document.querySelector('[data-currentOp]');
+
+
+
